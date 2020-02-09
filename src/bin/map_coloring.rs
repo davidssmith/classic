c7b396beff701800060e2459eec2b3f737840fb0
David S. Smith <david.smith@gmail.com>
2020-02-08 23:33:09 -0600

    CSP works, yay!

--- src/bin/map_coloring.rs
+++ src/bin/map_coloring.rs
@@ -1,47 +1,71 @@
+//use std::cmp::Eq;
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::cmp::Eq;
 
 pub trait Constraint<V: Eq + Hash, D> {
-    fn satisfied(&self, assignment: HashMap<V,D>) -> bool;
+    fn satisfied(&self, assignment: &HashMap<V, D>) -> bool;
     fn variables(&self) -> Vec<V>;
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct CSP<V,D,C> where V: Clone + Copy + Eq + Hash, C: Constraint<V,D> {
+pub struct CSP<V, D, C>
+where
+    V: Clone + Copy + Eq + Hash,
+    D: Clone + Copy,
+    C: Clone + Constraint<V, D>,
+{
     variables: Vec<V>,
-    domains: HashMap<V,Vec<D>>,
-    constraints: HashMap<V,Vec<C>>,
+    domains: HashMap<V, Vec<D>>,
+    constraints: HashMap<V, Vec<C>>,
 }
 
-impl<V: Clone + Copy + Eq + Hash, D: Clone, C: Constraint<V,D>> CSP<V,D,C> {
-    fn new(variables: Vec<V>, domains: HashMap<V, Vec<D>>) -> CSP<V,D,C> {
-        let mut constraints: HashMap<V,Vec<C>> = HashMap::new();
+impl<V: Clone + Copy + Eq + Hash, D: Clone + Copy, C: Clone + Constraint<V, D>> CSP<V, D, C> {
+    fn new(variables: Vec<V>, domains: HashMap<V, Vec<D>>) -> CSP<V, D, C> {
+        let mut constraints: HashMap<V, Vec<C>> = HashMap::new();
         for variable in &variables {
             constraints.insert(*variable, Vec::new());
             if !domains.contains_key(&variable) {
                 panic!("Every variable should have a domain assigned to it.");
             }
         }
-        CSP { variables, domains, constraints }
+        CSP {
+            variables,
+            domains,
+            constraints,
+        }
     }
     fn add_constraint(&mut self, constraint: C) {
-        for variable in &constraint.variables() {
+        let vars = constraint.variables();
+        for variable in vars {
             if !self.variables.contains(&variable) {
                 panic!("Variable in constraint not in CSP");
             } else {
-                self.constraints.get_mut(&variable).unwrap().push(constraint)
+                self.constraints
+                    .get_mut(&variable)
+                    .unwrap()
+                    .push(constraint.clone())
             }
         }
     }
-    fn consistent(&self, variable: V, assignment: &HashMap<V,D>) -> bool {
-        self.constraints[&variable].iter().any(|&c| !c.satisfied(*assignment))
+    fn consistent(&self, variable: V, assignment: &HashMap<V, D>) -> bool {
+        let constraints = self.constraints.get(&variable).unwrap();
+        for c in constraints {
+            if !c.satisfied(assignment) {
+                return false;
+            }
+        }
+        true
+            //.iter()
+            //.any(|&c| !*c.satisfied(*assignment))
     }
-    fn backtracking_search(&self, assignment: HashMap<V,D>) -> Option<HashMap<V,D>> {
+    fn backtracking_search(&self, assignment: HashMap<V, D>) -> Option<HashMap<V, D>> {
         if assignment.len() == self.variables.len() {
             return Some(assignment);
         }
-        let unassigned: Vec<V> = self.variables.clone().into_iter()
+        let unassigned: Vec<V> = self
+            .variables
+            .clone()
+            .into_iter()
             .filter(|&v| !assignment.contains_key(&v))
             .collect();
         let first = unassigned[0];
@@ -59,7 +83,7 @@ impl<V: Clone + Copy + Eq + Hash, D: Clone, C: Constraint<V,D>> CSP<V,D,C> {
 }
 
 /// Variables
-#[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Place {
     WA,
     NT,
@@ -71,7 +95,7 @@ enum Place {
 }
 
 /// Domains
-#[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Color {
     Red,
     Green,
@@ -79,12 +103,11 @@ enum Color {
 }
 
 /// Constraint: Proximity, touching can't have same color
-#[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
-struct MapColorConstraint(Place,Place);
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct MapColorConstraint(Place, Place);
 
-
-impl Constraint<Place,Color> for MapColorConstraint {
-    fn satisfied(&self, assignment: HashMap<Place,Color>) -> bool {
+impl Constraint<Place, Color> for MapColorConstraint {
+    fn satisfied(&self, assignment: &HashMap<Place, Color>) -> bool {
         if !assignment.contains_key(&self.0) || !assignment.contains_key(&self.1) {
             return true;
         }
@@ -96,14 +119,22 @@ impl Constraint<Place,Color> for MapColorConstraint {
 }
 
 fn main() {
-    let variables: Vec<Place> = vec![Place::WA, Place::NT, Place::SA, Place::Q, Place::NSW, Place::V, Place::T];
+    let variables: Vec<Place> = vec![
+        Place::WA,
+        Place::NT,
+        Place::SA,
+        Place::Q,
+        Place::NSW,
+        Place::V,
+        Place::T,
+    ];
     let mut domains: HashMap<Place, Vec<Color>> = HashMap::new();
     for &variable in variables.iter() {
         domains.insert(variable, vec![Color::Red, Color::Blue, Color::Green]);
     }
-    println!("variables: {:?}", variables);
-    println!("domains: {:?}", domains);
-    let mut csp: CSP<Place,Color,MapColorConstraint> = CSP::new(variables, domains);
+    //println!("variables: {:?}", variables);
+    //println!("domains: {:?}", domains);
+    let mut csp: CSP<Place, Color, MapColorConstraint> = CSP::new(variables, domains);
     csp.add_constraint(MapColorConstraint(Place::WA, Place::NT));
     csp.add_constraint(MapColorConstraint(Place::WA, Place::SA));
     csp.add_constraint(MapColorConstraint(Place::SA, Place::NT));
@@ -114,8 +145,8 @@ fn main() {
     csp.add_constraint(MapColorConstraint(Place::V, Place::SA));
     csp.add_constraint(MapColorConstraint(Place::V, Place::NSW));
     csp.add_constraint(MapColorConstraint(Place::V, Place::T));
-    let mut initial_guess: HashMap<Place,Color> = HashMap::new();
-    initial_guess.insert(Place::WA, Color::Blue);
+    let mut initial_guess: HashMap<Place, Color> = HashMap::new();
+    initial_guess.insert(Place::WA, Color::Red);
     let solution = csp.backtracking_search(initial_guess);
     if solution == None {
         println!("No solution found!");
--- src/csp.rs
+++ src/csp.rs
@@ -1,77 +1,80 @@
+//use std::cmp::Eq;
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub trait Constraint<V: Hash + Eq, D> {
-    fn satisfied(&self, assignment: HashMap<V, D>) -> bool;
+pub trait Constraint<V: Eq + Hash, D> {
+    fn satisfied(&self, assignment: &HashMap<V, D>) -> bool;
+    fn variables(&self) -> Vec<V>;
 }
 
-//V = TypeVar('V') # variable type
-//D = TypeVar('D') # domain type
-
-// A constraint satisfaction problem consists of variables of type V
-// that have ranges of values known as domains of type D and constraints
-// that determine whether a particular variable's domain selection is valid
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub struct CSP<V: Hash + Eq + PartialEq, D: Hash, C: Constraint<V,D>> {
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CSP<V, D, C>
+where
+    V: Clone + Copy + Eq + Hash,
+    D: Clone + Copy,
+    C: Clone + Constraint<V, D>,
+{
     variables: Vec<V>,
     domains: HashMap<V, Vec<D>>,
     constraints: HashMap<V, Vec<C>>,
 }
 
-
-impl<V: Hash + Eq + PartialEq, D> CSP<V,D> {
-    fn new(variables: Vec<V>, domains: HashMap<V, Vec<D>>) -> CSP<V,D> {
-        let mut constraints: HashMap<V,Vec<Constraint<V,D>>> = HashMap::new();
-        for variable in variables {
-            constraints.insert(variable, Vec::new());
-            if !self.domains.contains_key(variable) {
+impl<V: Clone + Copy + Eq + Hash, D: Clone + Copy, C: Clone + Constraint<V, D>> CSP<V, D, C> {
+    fn new(variables: Vec<V>, domains: HashMap<V, Vec<D>>) -> CSP<V, D, C> {
+        let mut constraints: HashMap<V, Vec<C>> = HashMap::new();
+        for variable in &variables {
+            constraints.insert(*variable, Vec::new());
+            if !domains.contains_key(&variable) {
                 panic!("Every variable should have a domain assigned to it.");
             }
         }
-        CSP { variables, domains, constraints }
+        CSP {
+            variables,
+            domains,
+            constraints,
+        }
     }
-    fn add_constraint(&self, constraint: Constraint<V,D>) {
-        for variable in constraint.variables.iter() {
-            if !self.variables.contains_kep(variable) {
+    fn add_constraint(&mut self, constraint: C) {
+        let vars = constraint.variables();
+        for variable in vars {
+            if !self.variables.contains(&variable) {
                 panic!("Variable in constraint not in CSP");
             } else {
-                self.constraints[variable].append(constraint)
+                self.constraints
+                    .get_mut(&variable)
+                    .unwrap()
+                    .push(constraint.clone())
             }
         }
     }
-
-    // Check if the value assignment is consistent by checking all constraints
-    // for the given variable against it
-    fn consistent(&self, variable: V, assignment: HashMap<V,D>) -> bool {
-        for constraint in self.constraints[variable] {
-            if !constraint.satisfied(assignment) {
+    fn consistent(&self, variable: V, assignment: &HashMap<V, D>) -> bool {
+        let constraints = self.constraints.get(&variable).unwrap();
+        for c in constraints {
+            if !c.satisfied(assignment) {
                 return false;
             }
         }
         true
+            //.iter()
+            //.any(|&c| !*c.satisfied(*assignment))
     }
-
-    fn backtracking_search(&self, assignment: HashMap<V,D>) -> Option<HashMap<V,D>> {
-        // assignment is complete if every variable is assigned (our base case)
+    fn backtracking_search(&self, assignment: HashMap<V, D>) -> Option<HashMap<V, D>> {
         if assignment.len() == self.variables.len() {
             return Some(assignment);
         }
-        // get all variables in the CSP but not in the assignment
-        let unassigned: Vec<V> = self.variables.iter()
-            .filter(|v| !assignment.contains_key(v))
+        let unassigned: Vec<V> = self
+            .variables
+            .clone()
+            .into_iter()
+            .filter(|&v| !assignment.contains_key(&v))
             .collect();
-
-        // get the every possible do    main value of the first unassigned variable
-        let first: V = unassigned[0];
-        for value in self.domains[first] {
-            let local_assignment = assignment.clone();
-            local_assignment[first] = value;
-            // if we're still consistent, we recurse (continue)
-            if self.consistent(first, local_assignment) {
-                let result = self.backtracking_search(local_assignment);
-                // if we didn't find the result, we will end up backtracking
-                if let Some(result) {
-                    return result.unwrap();
+        let first = unassigned[0];
+        for value in &self.domains[&first] {
+            let mut local_assignment = assignment.clone();
+            local_assignment.insert(first, *value);
+            if self.consistent(first, &local_assignment) {
+                if let Some(result) = self.backtracking_search(local_assignment) {
+                    return Some(result);
                 }
             }
         }
