6666e6e4a2eb6fb4f413a90705ab11512916855f
David S. Smith <david.smith@gmail.com>
2020-02-10 17:21:37 -0600

    c4 wip

--- src/bin/connect4.rs
+++ src/bin/connect4.rs
@@ -1,13 +1,15 @@
 //! connect 4 solver
-use std::cmp;
-extern crate board;
+use classic::minimax::find_best_move;
+use std::{cmp, fmt};
+extern crate classic;
 use classic::board::{Piece, Board, Move};
+use text_io::read;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum C4Piece {
-    B, // "X"
-    R, // "O"
-    E, // " "
+    B,
+    R,
+    E,
 }
 
 impl Piece for C4Piece {
@@ -35,13 +37,13 @@ impl fmt::Display for C4Piece {
 }
 
 
-fn generate_segments(num_columns: i32, num_rows: i32, segment_length: i32) -> Vec<Vec<(i32,i32)>> {
-    let segments: Vec<Vec<(i32,i32)>> = Vec::new();
+fn generate_segments(num_columns: u8, num_rows: u8, segment_length: u8) -> Vec<Vec<(u8,u8)>> {
+    let segments: Vec<Vec<(u8,u8)>> = Vec::new();
     // generate the vertical segments
     for c in 0..num_columns {
-        for r in 0..num_rows - segment_length + 1 {
-            let mut segment: Vec<(i32,i32)> = Vec::new();
-            for t in 0..segment_length {
+        for r in 0..(NUM_ROWS - SEGMENT_LENGTH + 1) {
+            let mut segment: Vec<(u8,u8)> = Vec::new();
+            for t in 0..SEGMENT_LENGTH {
                 segment.push((c, r + t));
             }
             segments.push(segment);
@@ -49,10 +51,10 @@ fn generate_segments(num_columns: i32, num_rows: i32, segment_length: i32) -> Ve
     }
 
     // generate the horizontal segments
-    for c in 0..num_columns - segment_length + 1 {
-        for r in 0..num_rows {
-            let mut segment: Vec<(i32,i32)> = Vec::new();
-            for t in 0..segment_length {
+    for c in 0..(num_columns - SEGMENT_LENGTH + 1) {
+        for r in 0..NUM_ROWS {
+            let mut segment: Vec<(u8,u8)> = Vec::new();
+            for t in 0..SEGMENT_LENGTH {
                 segment.push((c + t, r));
             }
             segments.push(segment);
@@ -60,10 +62,10 @@ fn generate_segments(num_columns: i32, num_rows: i32, segment_length: i32) -> Ve
     }
 
     // generate the bottom left to top right diagonal segments
-    for c in 0..num_columns - segment_length + 1 {
-        for r in 0..num_rows - segment_length + 1 {
-            let mut segment: Vec<(i32,i32)> = Vec::new();
-            for t in 0..segment_length {
+    for c in 0..(num_columns - SEGMENT_LENGTH + 1) {
+        for r in 0..NUM_ROWS - SEGMENT_LENGTH + 1 {
+            let mut segment: Vec<(u8,u8)> = Vec::new();
+            for t in 0..SEGMENT_LENGTH {
                 segment.push((c + t, r + t));
             }
             segments.push(segment);
@@ -71,10 +73,10 @@ fn generate_segments(num_columns: i32, num_rows: i32, segment_length: i32) -> Ve
     }
 
     // generate the top left to bottom right diagonal segments
-    for c in 0..num_columns - segment_length + 1 {
-        for r in 0..segment_length - 1, num_rows {
-            let mut segment: Vec<(i32,i32)> = Vec::new();
-            for t in 0..segment_length {
+    for c in 0..num_columns - SEGMENT_LENGTH + 1 {
+        for r in (SEGMENT_LENGTH - 1)..NUM_ROWS {
+            let mut segment: Vec<(u8,u8)> = Vec::new();
+            for t in 0..SEGMENT_LENGTH {
                 segment.push((c + t, r - t));
             }
             segments.push(segment);
@@ -83,137 +85,148 @@ fn generate_segments(num_columns: i32, num_rows: i32, segment_length: i32) -> Ve
     segments
 }
 
-#[derive(Clone,Copy,Debug,Eq,PartialEq)]
-struct C4Board {
-    num_rows: i32, // = 6
-    num_cols: i32, // = 7
-    segment_length: i32, // = 4
-    segments: Vec<Vec<(i32,i32)>>,
-    // = generate_segments(num_cols, num_rows, segment_length)
-}
 
-impl C4Board {
-fn new(position: Option<Vec<C4Column>>, turn: C4Piece = C4Piece::B) {
-    if position is None:
-        self.position: Vec<C4Column> = [C4Column::new() for _ in 0..C4Board.num_cols)]
-    else:
-        self.position = position
-    self.turn: C4Piece = turn
-}
-/// Returns the count of black & red pieces in a segment
-fn count_segment(&self, segment: Vec<(i32,i32)>) -> (i32,i32) {
-    let mut black_count: i32 = 0;
-    let mut red_count: i32 = 0;
-    for (column, row) in segment {
-        if self.position[column][row] == C4Piece::B {
-            black_count += 1;
-        } else if self.position[column][row] == C4Piece::R {
-            red_count += 1;
-        }
-    }
-    return (black_count, red_count)
-}
-fn evaluate_segment(self, segment: Vec<(i32,i32)>, player: Piece) -> f32 {
-    let (black_count, red_count) = self.count_segment(segment);
-    if red_count > 0 && black_count > 0 {
-        return 0; // mixed segments are neutral
-    }
-    let count = cmp::max(red_count, black_count);
-    let mut score: f32 = 0.0;
-    if count == 2 {
-        score = 1;
-    } else if count == 3 {
-        score = 100;
-    } else if count == 4 {
-        score = 1000000;
-    }
-    let mut color: C4Piece = C4Piece::B;
-    if red_count > black_count {
-        color = C4Piece::R;
-    }
-    if color != player {
-        return -score;
-    }
-    score
-}
-}
-
-impl Board<C4Piece> for C4Board {
-fn turn(&self) -> C4Piece {
-     self.turn
-}
-
-fn make_move(&self, location: Move) -> C4Board {
-    temp_position: Vec[C4Board.Column] = self.position.clone();
-    for c in 0..C4Board.num_cols):
-        temp_position[c] = self.position[c].copy();
-    temp_position[location].push(self.turn);
-     C4Board(temp_position, self.turn.opposite)
- }
-
-fn legal_moves(&self) -> Vec<Move> {
-    return [Move(c) for c in 0..C4Board.num_cols) if not self.position[c].full]
-}
-
-fn is_win(&self) -> bool {
-    for segment in C4Board.segments {
-        let (black_count, red_count) = self.count_segment(segment);
-        if black_count == 4 || red_count == 4 {
-            return true;
-        }
-    }
-     false
- }
-
-
-
-fn evaluate(&self, player: Piece) -> f32 {
-    let mut total: f32 = 0.0;
-    for segment in C4Board.segments {
-        total += self.evaluate_segment(segment, player);
-    }
-    return total;
-}
-}
-
-
-def __repr__(&self) -> str:
-    display: str = ""
-    for r in reversed(0..C4Board.num_rows)):
-        display += "|"
-        for c in 0..C4Board.num_cols):
-            display += f"{self.position[c][r]}" + "|"
-        display += "\n"
-    return display
-
-#[derive(Clone,Copy,Debug,Eq,PartialEq)]
+#[derive(Clone,Debug,Eq,PartialEq)]
 struct C4Column(Vec<C4Piece>);
 
 impl C4Column {
-        fn new() -> C4Column {
-            C4Column(Vec::new())
+    fn new() -> C4Column {
+        C4Column(Vec::new())
+    }
+
+    fn full(&self) -> bool {
+         self.0.len() == NUM_ROWS as usize
+     }
+
+    fn push(&self, item: C4Piece) {
+        if self.full() {
+            panic!("Trying to push piece to full column");
         }
+        self.0.push(item);
+    }
 
-        fn full(&self) -> bool {
-             self.0.len() == C4Board.num_rows
-         }
-
-        fn push(&self, item: C4Piece) {
-            if self.full() {
-                panic!("Trying to push piece to full column");
-            }
-            self.0.push(item);
+    fn get(&self, index: u8) -> C4Piece {
+        if index as usize > self.0.len() - 1 {
+            return C4Piece::E
         }
-
-        fn __getitem__(&self, index: i32) -> C4Piece {
-            if index > self.0.len() - 1 {
-                return C4Piece::E
-            }
-            self.0[index]
-        }
-
+        self.0[index as usize]
+    }
 }
 
+const NUM_ROWS: u8 = 6;
+const NUM_COLS: u8 = 7;
+const SEGMENT_LENGTH: u8 = 4;
+
+#[derive(Clone,Debug,Eq,PartialEq)]
+struct C4Board {
+    position: Vec<C4Column>,
+    turn: C4Piece,
+    segments: Vec<Vec<(u8,u8)>>,
+}
+
+impl C4Board {
+    fn new(position: Option<Vec<C4Column>>, turn: C4Piece) -> C4Board {
+        // turn default: C4Piece::B
+        let position = match position {
+            None => (0..NUM_COLS).map(|_| C4Column::new()).collect::<Vec<_>>(),
+            Some(p) => p,
+        };
+        let segments: Vec<Vec<(u8,u8)>> = generate_segments(NUM_COLS, NUM_ROWS, SEGMENT_LENGTH);
+        C4Board { position, turn, segments }
+
+    }
+    /// Returns the count of black & red pieces in a segment
+    fn count_segment(&self, segment: Vec<(u8,u8)>) -> (u8,u8) {
+        let mut black_count: u8 = 0;
+        let mut red_count: u8 = 0;
+        for (column, row) in segment {
+            if self.position[column as usize][row as usize] == C4Piece::B {
+                black_count += 1;
+            } else if self.position[column as usize][row as usize] == C4Piece::R {
+                red_count += 1;
+            }
+        }
+        return (black_count, red_count)
+    }
+    fn evaluate_segment(self, segment: Vec<(u8,u8)>, player: C4Piece) -> f32 {
+        let (black_count, red_count) = self.count_segment(segment);
+        if red_count > 0 && black_count > 0 {
+            return 0.0; // mixed segments are neutral
+        }
+        let count = cmp::max(red_count, black_count);
+        let mut score: f32 = 0.0;
+        if count == 2 {
+            score = 1.0;
+        } else if count == 3 {
+            score = 100.0;
+        } else if count == 4 {
+            score = 1000000.0;
+        }
+        let mut color: C4Piece = C4Piece::B;
+        if red_count > black_count {
+            color = C4Piece::R;
+        }
+        if color != player {
+            return -score;
+        }
+        score
+    }
+}
+
+impl Board<C4Piece> for C4Board {
+    fn turn(&self) -> C4Piece {
+         self.turn
+    }
+
+    fn make_move(&self, location: Move) -> C4Board {
+        let temp_position: Vec<C4Column> = self.position.clone();
+        for c in 0..NUM_COLS {
+            temp_position[c as usize] = self.position[c as usize].clone();
+        }
+        temp_position[location as usize].push(self.turn);
+         C4Board { position: temp_position, turn: self.turn.opposite(), segments: self.segments.clone() }
+     }
+
+    fn legal_moves(&self) -> Vec<Move> {
+        (0..NUM_COLS).filter(|&c| !self.position[c as usize].full()).collect::<Vec<_>>()
+    }
+
+    fn is_win(&self) -> bool {
+        for segment in self.segments {
+            let (black_count, red_count) = self.count_segment(segment);
+            if black_count == 4 || red_count == 4 {
+                return true;
+            }
+        }
+         false
+     }
+
+
+
+    fn evaluate(&self, player: C4Piece) -> f32 {
+        let mut total: f32 = 0.0;
+        for segment in self.segments {
+            total += self.evaluate_segment(segment, player);
+        }
+        return total;
+    }
+}
+
+
+impl fmt::Display for C4Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut display = String::new();
+        for r in (0..NUM_ROWS).reversed() {
+            display.push("|".to_string());
+            for c in 0..NUM_COLS {
+                display.push(format!("{}", self.position[c as usize][r as usize]));
+                display.push("|".to_string());
+            }
+            display.push("\n".to_string());
+        }
+        write!(f, "{}", display)
+    }
+}
 
 
 fn get_player_move<B: Board<P>, P: Piece>(board: &B) -> Move {
@@ -228,10 +241,7 @@ fn get_player_move<B: Board<P>, P: Piece>(board: &B) -> Move {
 
 fn main() {
     // main game loop
-    let mut board = C4Board {
-        position: (0..9).map(|_| C4Piece::E).collect(),
-        turn: C4Piece::X,
-    };
+    let mut board = C4Board::new((0..9).map(|_| C4Piece::E).collect::<Vec<_>>(), C4Piece::R);
     loop {
         let human_move = get_player_move(&board);
         board = board.make_move(human_move);
